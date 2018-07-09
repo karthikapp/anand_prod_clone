bf354875c77a0acdd4724204bcecf686b0356e61
karthikapp <karthik.uic@gmail.com>
2018-07-10 00:52:33 +0530

    newchanges

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseserviceService } from '../firebaseservice.service'
 declare var jQuery: any;
 import * as firebase from 'firebase';
+import 'firebase/firestore';
+import { AngularFirestore } from 'angularfire2/firestore';
+
 
 @Component({
 	selector: 'app-category',
@@ -14,14 +17,25 @@ export class CategoryComponent implements OnInit {
 	public items : any
 	public id?: string;
 	public categoryname?: string;
-	public hassubcategory?: boolean;
+	public hassubcategory?: any;
 	public subcategory?: any[];
 	public createtime?: string;
 	public edittime?: string;
+	public datetime?: any;
+	public datetimec?: any;
+	public addsubFields: {
+		subcategoryname : string,
+		hassubcategory: any,
+		createtime: any,
+		edittime: any
+	}[] = []
 
 	constructor(public db: FirebaseserviceService) 
 	{ 
-
+		this.hassubcategory = false;
+		this.datetime = firebase.firestore.FieldValue.serverTimestamp();
+		this.datetimec = firebase.database.ServerValue.TIMESTAMP;
+		//this.addsubFields[0].hassubcategoryl1 = false 
 		
 	}
 
@@ -39,15 +53,89 @@ export class CategoryComponent implements OnInit {
 
   addcategoryobject()
   {
+  	// if(this.hassubcategory == 'Yes')
+  	// {
+  	// 	this.hassubcategory = true
+  	// }
+  	// else
+  	// {
+  	// 	this.subcategory = false
+  	// }
+  	
   	var objecttopush = 
   	{
 	"categoryname" : this.categoryname,
 	"hassubcategory" : this.hassubcategory,
-	"createtime": firebase.firestore.FieldValue.serverTimestamp(),
-	"edittime" : firebase.firestore.FieldValue.serverTimestamp()
+	"createtime": this.datetime,
+	"edittime" : this.datetime
   	}
 
-  	this.db.addcategory(objecttopush)
+  	console.log("addsubFields", this.addsubFields)
+
+
+
+  	this.db.addcategory(objecttopush, this.addsubFields).then(success => {
+  		alert("added successfully");
+  		this.discardCategory();
+  	})
+  }
+
+  discardCategory(){
+  	this.addsubFields = []
+  	this.categoryname = '';
+  	this.hassubcategory = false;
+  }
+
+  updateCategory(itemid){
+  	console.log(itemid)
+  	this.db.updateCategory(itemid)
+  }
+
+  updateSubCategory(itemid, subitemid){
+  	console.log(subitemid)
+  	this.db.updateSubCategory(itemid, subitemid)
+  }
+
+  deleteCategory(itemid){
+  	console.log(itemid)
+  	this.db.deleteCategory(itemid)
+  }
+
+  deleteSubCategory(itemid, subitemid){
+  	console.log(subitemid)
+
+  	this.db.deleteSubCategory(itemid,subitemid)
+  }
+
+  addSubCategory(index, hassubcategory){
+  	this.hassubcategory = hassubcategory
+  	if(hassubcategory == true)
+  	{
+  		this.addsubFields.push({ subcategoryname:'', 
+  			hassubcategory: false,
+  			createtime: this.datetimec,
+  			edittime: this.datetimec});
+  	}
+  	else
+  	{
+  		this.addsubFields.splice(index,1);
+  	}
+  }
+
+  addSubCategoryLoop(index,  hassubcategory){
+  	this.addsubFields[index].hassubcategory = hassubcategory
+
+  	if(hassubcategory == true)
+  	{
+  		this.addsubFields.push({ subcategoryname:'', 
+  			hassubcategory: false,
+  			createtime: this.datetimec,
+  			edittime: this.datetimec});
+  	}
+  	else
+  	{
+  		this.addsubFields.splice(index+1,1);
+  	}
   }
 
 
--- src/app/firebaseservice.service.ts
+++ src/app/firebaseservice.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument , AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
 import { CategoryItem } from './models/categoryitem'
+import 'firebase/firestore';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class FirebaseserviceService {
   
   CategoryCollection: AngularFirestoreCollection<CategoryItem>
-  public items : Observable<CategoryItem[]>
+  public category : Observable<CategoryItem[]>
+  public categoryDoc: AngularFirestoreDocument<CategoryItem>;
 
   constructor(public db: AngularFirestore)
 
@@ -16,15 +20,66 @@ export class FirebaseserviceService {
 
   showcollectios()
   {
-  	this.items = this.db.collection('category').valueChanges()
-  	return this.items
+  	// this.category = this.db.collection('category').valueChanges()
+  	// return this.category
+
+     this.CategoryCollection = this.db.collection<CategoryItem>('category');
+    // .snapshotChanges() returns a DocumentChangeAction[], which contains
+    // a lot of information about "what happened" with each change. If you want to
+    // get the data and the id use the map operator.
+    this.category = this.CategoryCollection.snapshotChanges()
+    .map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data() as CategoryItem;
+        const id = a.payload.doc.id;
+        console.log(id, data)
+        return { id, ...data };
+      });
+    });
+      //console.log("cat", this.category)
+      return this.category
   }
 
 
-  addcategory(categoryobject)
+  addcategory(categoryobject, subcategoryobject)
   {
+    var item ={
+      categoryname: categoryobject.categoryname,
+      hassubcategory: categoryobject.hassubcategory,
+      createtime: categoryobject.createtime,
+      edittime: categoryobject.edittime,
+      subcategory: subcategoryobject
+    }
+
+    console.log("item", item)
     const listRef = this.db.collection('category')
-    return listRef.add(categoryobject)
+
+    return listRef.add(item)
+  }
+
+  updateCategory(categoryid){
+
+  }
+
+  deleteCategory(categoryid){
+    this.categoryDoc = this.db.collection('category').doc(categoryid);
+    console.log(this.categoryDoc)
+
+
+    this.categoryDoc.delete();
+  }
+
+  updateSubCategory(categoryid, subcategoryid){
+
+  }
+
+  deleteSubCategory(categoryid, subcategoryid){
+//     var categoryRef = this.db.collection('category').doc(categoryid);
+
+// // Remove the 'capital' field from the document
+//     var removeSubCategory = categoryRef.update({
+//     subcategory: firebase.firestore.FieldValue.delete()
+// });
   }
 
 }
