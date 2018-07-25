01c8c229a799011fdaa834aea78bd9684ab91682
karthikapp <karthik.uic@gmail.com>
2018-07-25 18:57:42 +0530

    subcat4leveldeep

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,8 +1,18 @@
 export const firebaseConfig = {
- apiKey: "AIzaSyAkEou3EOwCdOGs4UvpMjsHBCbZqZu-kEg",
-    authDomain: "somenew-65a21.firebaseapp.com",
-    databaseURL: "https://somenew-65a21.firebaseio.com",
-    projectId: "somenew-65a21",
-    storageBucket: "somenew-65a21.appspot.com",
-    messagingSenderId: "891982288558"
+
+  apiKey: "AIzaSyCVdEELI0k-idQW_S030NeER9Oe8L-FCfA",
+    authDomain: "raksha-db.firebaseapp.com",
+    databaseURL: "https://raksha-db.firebaseio.com",
+    projectId: "raksha-db",
+    storageBucket: "raksha-db.appspot.com",
+    messagingSenderId: "59012078689"
 };
+
+
+//Karthik firebase
+ // apiKey: "AIzaSyAkEou3EOwCdOGs4UvpMjsHBCbZqZu-kEg",
+ //    authDomain: "somenew-65a21.firebaseapp.com",
+ //    databaseURL: "https://somenew-65a21.firebaseio.com",
+ //    projectId: "somenew-65a21",
+ //    storageBucket: "somenew-65a21.appspot.com",
+ //    messagingSenderId: "891982288558"
